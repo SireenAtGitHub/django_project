d697ac3159fc368aaec46a9e5fa8002a2cf56852
SireenAtGithub <er.sireen@gmail.com>
2022-04-02 17:27:39 +0530

    1st Day Completed with database

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,16 @@
 from django.shortcuts import render
+from . models import Post
+
 # username = sireengothadiya
 # password = testing321
-posts = [
-	{
-		'author' : 'Sireen',
-		'title' : 'Blog First Post',
-		'content' : 'Content goes here.. blah blah blah',
-		'date_posted' : 'July 20, 2000'
-	},
-	{
-		'author' : 'Maitri',
-		'title' : 'Blog Second Post',
-		'content' : 'New second content blah blah blah',
-		'date_posted' : 'January 23, 2000'
-	}
-]
+
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
+
 def about(request):
-	return render(request, 'blog/about.html',{'title' : 'About'})
-# Create your views here
+	return render(request, 'blog/about.html', {'title': 'About'})
