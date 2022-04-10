e7e8f56a6e8d8778204160f05b2e70e508fd4fbb
SireenAtGithub <er.sireen@gmail.com>
2022-04-10 16:58:53 +0530

    Completed Pagination and Posts by single user functionalities

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from abc import ABC
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
@@ -20,6 +21,18 @@ class PostListView(ListView):
     template_name = "blog/home.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
