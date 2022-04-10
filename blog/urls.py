157663c0863d8ee2f5701f585b97adb7bd3a5729
SireenAtGithub <er.sireen@gmail.com>
2022-04-10 13:25:32 +0530

    Completed Delete Functionalities

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from . views import *
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from .models import Post
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
-                                  UpdateView
+                                  UpdateView,
+                                  DeleteView
                                   )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -41,6 +42,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
