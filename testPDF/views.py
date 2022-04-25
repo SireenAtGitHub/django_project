490437e19611fb1c3e2cd3723ab5d72cc6f88b60
SireenAtGithub <er.sireen@gmail.com>
2022-04-25 21:20:30 +0530

    Testing PDF

--- testPDF/views.py
+++ testPDF/views.py
@@ -52,5 +52,5 @@ def invoice(request):
 
 def register_font():
     pdfmetrics.registerFont(TTFont('CenturyGothicBold',  os.path.abspath(os.getcwd()+'/static/CenturyGothicBold.ttf')))
-    pdfmetrics.registerFont(TTFont('CenturyGothicRegular', os.path.abspath(os.getcwd()+'\static\CenturyGothicRegular.ttf')))
+    pdfmetrics.registerFont(TTFont('CenturyGothicRegular', os.path.abspath(os.getcwd()+'/static/CenturyGothicRegular.ttf')))
     registerFontFamily('CenturyGothic', normal='CGRegular', bold='CGBold')
